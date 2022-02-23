6501c8a2b219db3583effbc7146dcb199682c36b
winstonwang <wwang@lcs.on.ca>
2022-02-23 09:47:34 -0500

    Initial Commit

---
+++ QuotesFinder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QuotesFinder
+//
+//  Created by Winston Wang on 2022-02-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QuotesFinder/QuotesFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesFinderApp.swift
+//  QuotesFinder
+//
+//  Created by Winston Wang on 2022-02-23.
+//
+
+import SwiftUI
+
+@main
+struct QuotesFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
